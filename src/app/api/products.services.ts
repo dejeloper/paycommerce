78f065765f390ee962ed225329db242476a09589
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-08-11 14:59:18 -0500

    perf: Add runInInjectionContext for inject context

--- src/app/api/products.services.ts
+++ src/app/api/products.services.ts
@@ -1,14 +1,22 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable, signal } from '@angular/core';
+import {
+  EnvironmentInjector,
+  inject,
+  Injectable,
+  runInInjectionContext,
+  signal,
+} from '@angular/core';
 import { environment } from '@envs/environment.development';
 import { Product } from '@shared/models/product.interface';
 import { tap } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Injectable({ providedIn: 'root' })
 export class ProductServices {
   public products = signal<Product[]>([]);
   private readonly _http = inject(HttpClient);
   private readonly _endPoint = environment.apiURL;
+  private readonly _injector = inject(EnvironmentInjector);
 
   constructor() {
     this.getProducts();
@@ -22,6 +30,13 @@ export class ProductServices {
   }
 
   public getProductById(id: number) {
-    return this._http.get<Product>(`${this._endPoint}/products/${id}`);
+    const product$ = this._http.get<Product>(
+      `${this._endPoint}/products/${id}`
+    );
+
+    return runInInjectionContext(this._injector, () =>
+      toSignal<Product>(product$)
+    );
+    // return this._http.get<Product>(`${this._endPoint}/products/${id}`);
   }
 }
