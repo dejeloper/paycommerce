f9b589b18d40ccaa2c98b8a9d16d262a058d7887
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-08-11 10:58:58 -0500

    feat: Add header and logos

---
+++ src/app/layout/cart-logo/cart-logo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-logo',
+  standalone: true,
+  imports: [],
+  templateUrl: './cart-logo.component.svg',
+  styles: '',
+})
+export class CartLogoComponent {}
--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { CartLogoComponent } from '../cart-logo/cart-logo.component';
+import { LogoComponent } from '../logo/logo.component';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CartLogoComponent, LogoComponent],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: Event) {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.relative')) {
+      this.isMenuOpen = false;
+    }
+  }
 }
